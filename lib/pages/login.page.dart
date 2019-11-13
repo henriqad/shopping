db59ff4b60c43b5b8bcb59a68381fcc5f2edafb8
unknown <henriqad@gmail.com>
2019-11-13 01:43:34 -0300

    initial commit

---
+++ lib/pages/login.page.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Color(0xFFF5F5F5),
+        padding: EdgeInsets.only(top: 80, left: 20, right: 20, bottom: 40),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 450,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  new BoxShadow(
+                    color: Colors.black12,
+                    offset: new Offset(1, 2.0),
+                    blurRadius: 5,
+                    spreadRadius: 1,
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(15),
+                child: ListView(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              'Welcome',
+                              style: TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.w500),
+                            ),
+                            Text('Sign in to continue'),
+                          ],
+                        ),
+                        FlatButton(
+                          child: Text('Sign Up'),
+                          onPressed: () {},
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 60),
+                    TextFormField(
+                      //autofocus: true,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
+                        ),
+                      ),
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      //autofocus: true,
+                      keyboardType: TextInputType.text,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
+                        ),
+                      ),
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Container(
+                      height: 40,
+                      alignment: Alignment.centerRight,
+                      child: FlatButton(
+                        child: Text('Forgot your password?'),
+                        onPressed: () {},
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5),
+                        ),
+                      ),
+                      child: FlatButton(
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 15, bottom: 15),
+              child: Text(
+                '- OR -',
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
