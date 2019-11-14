e2a9bdffc46ddded8deaef4b1f9e9c5215d2a8dc
unknown <henriqad@gmail.com>
2019-11-14 01:31:55 -0300

    shopping cart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping/pages/login.page.dart';
+import 'pages/tabs.page.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,7 +12,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: DefaultTabController(
+        length: 3,
+        child: TabsPage(),
+      ),
     );
   }
 }
---
+++ lib/pages/tabs.page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:shopping/pages/cart.page.dart';
+import 'package:shopping/pages/home.page.dart';
+import 'package:shopping/pages/login.page.dart';
+
+class TabsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TabBarView(
+        children: <Widget>[
+          HomePage(),
+          CartPage(),
+          LoginPage(),
+        ],
+      ),
+      bottomNavigationBar: new TabBar(
+        tabs: <Widget>[
+          Tab(icon: new Icon(Icons.home)
+              /*, text: 'Home' */),
+          Tab(icon: new Icon(Icons.shopping_cart)),
+          Tab(icon: new Icon(Icons.perm_identity)),
+        ],
+        labelColor: Colors.blue,
+        unselectedLabelColor: Colors.black38,
+        indicatorSize: TabBarIndicatorSize.label,
+        indicatorPadding: EdgeInsets.all(5.0),
+        indicatorColor: Colors.blue,
+      ),
+    );
+  }
+}
